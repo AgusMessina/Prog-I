d56f424669d1be5a7302ddd37d5fbad63efcc3c9
beluuun <belengodoy2305@gmail.com>
2025-09-05 11:27:00 -0300

    tp5/

--- Belu/punto 1/main.c
+++ Belu/punto 1/main.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define MAX 30
 
-typedef struct{
-        char nombre[MAX];
-        int edad;
-        float altura;
-        float peso;
-        long DNI;
-    }Persona;
-
-void carga(Persona per){
-    printf("Ingrese nombre:\n");
-    scanf("%s", per.nombre);
-    printf("Ingrese su edad:\n");
-    scanf("%d", &per.edad);
-    printf("Ingrese altura:\n");
-    scanf("%f", &per.altura);
-    printf("Ingrese peso:\n");
-    scanf("%f", &per.peso);
-    printf("Ingrese DNI:\n");
-    scanf("%ld", &per.DNI);
-}
-
-void imprimir(Persona poblacion[], Persona per){
-    strcpy(poblacion[3].nombre, per.nombre);
-    poblacion[3].edad = per.edad;
-    poblacion[3].altura = per.altura;
-    poblacion[3].peso = per.peso;
-    poblacion[3].DNI = per.DNI;
-
-    printf("Nombre: %s\n", poblacion[3].nombre);
-    printf("Edad: %d\n", poblacion[3].edad);
-    printf("Altura: %f\n", poblacion[3].altura);
+int naturales(int n){
+    if(n == 1)
+        return 1;
+    else
+        printf("%d ", n);
+        return naturales(n-1);
 }
 
 int main()
 {
-    Persona per, poblacion[MAX];
+    int n;
 
-    carga(per);
-    imprimir(poblacion, per);
-
+    printf("Ingrese un numero natural:\n");
+    scanf("%d", &n);
+    while(n < 0){
+        printf("No es un numero natural..\n");
+        scanf("%d", &n);
+    }
+    printf("%d", naturales(n));
 }
